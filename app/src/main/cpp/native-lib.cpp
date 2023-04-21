7e7454c9ea1f23f3689ddf0c6989e309f237b8e2
Peter Spalthoff <peter.spalthoff@gmail.com>
2023-04-21 21:08:51 +0900

    Cleanup and comments added

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <dlfcn.h>
 
 #include "android/log.h"
 
-#include <dlfcn.h>
 #include "CL/cl.h"
 
+// OpenCL.so読込のための型の定義
 namespace cl {
     typedef cl_int(CL_API_CALL *PFN_clGetPlatformIDs)(
             cl_uint /* num_entries */, cl_platform_id * /* platforms */,
@@ -43,70 +44,55 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_android_example_opencl_1keyence_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    const char* source =
-            "__constant sampler_t smp_none = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_NONE | CLK_FILTER_NEAREST;\n"
-            "__constant sampler_t smp_zero = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;\n"
-            "#pragma OPENCL EXTENSION cl_khr_fp16 : enable\n"
-            "__kernel void bhwc_to_tensor(__global float* src, __write_only image2d_t tensor_image2d,\n"
-            "  int4 shared_int4_0,"
-            "  int4 shared_int4_1) {"
-            "  int linear_id = get_global_id(0);"
-            "  int x = linear_id / shared_int4_0.x;"
-            "  int b = linear_id % shared_int4_0.x;"
-            "  int y = get_global_id(1);"
-            "  int d = get_global_id(2);"
-            "  if (x >= shared_int4_1.x || y >= shared_int4_0.z || d >= shared_int4_0.w) return;"
-            "  half4 result;"
-            "  int c = d * 4;"
-            "  int index = ((b * shared_int4_0.z + y) * shared_int4_1.x + x) * shared_int4_0.y + c;"
-            "  result.x = src[index];"
-            "  result.y = c + 1 < shared_int4_0.y ? src[index + 1] : 1;"
-            "  result.z = c + 2 < shared_int4_0.y ? src[index + 2] : 2;"
-            "  result.w = c + 3 < shared_int4_0.y ? src[index + 3] : 3;"
-            "  write_imageh(tensor_image2d, (int2)(((x) * shared_int4_0.x + (b)), ((y) * shared_int4_0.w + (d))), result);"
-            "}";
-#ifdef PIXEL
-    void* opencllib = dlopen("libOpenCL-pixel.so", RTLD_NOW | RTLD_LOCAL);
-    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "ptr: %p", opencllib);
-    typedef void (*enableOpenCL_t)();
-    enableOpenCL_t enableOpenCL =
-            reinterpret_cast<enableOpenCL_t>(dlsym(opencllib, "enableOpenCL"));
-    enableOpenCL();
-
-    typedef void* (*loadOpenCLPointer_t)(const char* name);
-    loadOpenCLPointer_t loadOpenCLPointer;
-    loadOpenCLPointer = reinterpret_cast<loadOpenCLPointer_t>(
-            dlsym(opencllib, "loadOpenCLPointer"));
-
-    cl::clGetPlatformIDs = reinterpret_cast<cl::PFN_clGetPlatformIDs>(loadOpenCLPointer("clGetPlatformIDs"));
-    cl::clGetDeviceIDs = reinterpret_cast<cl::PFN_clGetDeviceIDs>(loadOpenCLPointer("clGetDeviceIDs"));
-    cl::clCreateContext = reinterpret_cast<cl::PFN_clCreateContext>(loadOpenCLPointer("clCreateContext"));
-    cl::clCreateProgramWithSource = reinterpret_cast<cl::PFN_clCreateProgramWithSource>(loadOpenCLPointer("clCreateProgramWithSource"));
-    cl::clBuildProgram = reinterpret_cast<cl::PFN_clBuildProgram>(loadOpenCLPointer("clBuildProgram"));
-#else
+    // OpenCL.so読込
     void* opencllib = dlopen("libOpenCL.so", RTLD_NOW | RTLD_LOCAL);
     cl::clGetPlatformIDs = reinterpret_cast<cl::PFN_clGetPlatformIDs>(dlsym(opencllib, "clGetPlatformIDs"));
     cl::clGetDeviceIDs = reinterpret_cast<cl::PFN_clGetDeviceIDs>(dlsym(opencllib, "clGetDeviceIDs"));
     cl::clCreateContext = reinterpret_cast<cl::PFN_clCreateContext>(dlsym(opencllib, "clCreateContext"));
     cl::clCreateProgramWithSource = reinterpret_cast<cl::PFN_clCreateProgramWithSource>(dlsym(opencllib, "clCreateProgramWithSource"));
     cl::clBuildProgram = reinterpret_cast<cl::PFN_clBuildProgram>(dlsym(opencllib, "clBuildProgram"));
-#endif
 
-    cl_uint num_platforms;
-    cl_platform_id platform;
     int err;
 
+    // OpenCLの初期化
+    cl_uint num_platforms;
+    cl_platform_id platform;
     err = cl::clGetPlatformIDs(1, &platform, &num_platforms);
-    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "Error: %i\n", err);
-
+    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "clGetPlatformIDs result: %i\n", err);
     cl_device_id device_id;
     err = cl::clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "Error: %i\n", err);
+    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "clGetDeviceIDs result: %i\n", err);
     cl_context context = cl::clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "Error: %i\n", err);
+    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "clCreateContext result: %i\n", err);
+
+    const char* source =
+        "__constant sampler_t smp_none = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_NONE | CLK_FILTER_NEAREST;\n"
+        "__constant sampler_t smp_zero = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;\n"
+        "#pragma OPENCL EXTENSION cl_khr_fp16 : enable\n"
+        "__kernel void bhwc_to_tensor(__global float* src, __write_only image2d_t tensor_image2d,\n"
+        "  int4 shared_int4_0,"
+        "  int4 shared_int4_1) {"
+        "  int linear_id = get_global_id(0);"
+        "  int x = linear_id / shared_int4_0.x;"
+        "  int b = linear_id % shared_int4_0.x;"
+        "  int y = get_global_id(1);"
+        "  int d = get_global_id(2);"
+        "  if (x >= shared_int4_1.x || y >= shared_int4_0.z || d >= shared_int4_0.w) return;"
+        "  half4 result;"
+        "  int c = d * 4;"
+        "  int index = ((b * shared_int4_0.z + y) * shared_int4_1.x + x) * shared_int4_0.y + c;"
+        "  result.x = src[index];"
+        "  result.y = c + 1 < shared_int4_0.y ? src[index + 1] : 1;"
+        "  result.z = c + 2 < shared_int4_0.y ? src[index + 2] : 2;"
+        "  result.w = c + 3 < shared_int4_0.y ? src[index + 3] : 3;"
+        "  write_imageh(tensor_image2d, (int2)(((x) * shared_int4_0.x + (b)), ((y) * shared_int4_0.w + (d))), result);"
+        "}";
     cl_program program = cl::clCreateProgramWithSource(context, 1, &source, NULL, &err);
+    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "clCreateProgramWithSource result: %i\n", err);
+
+    // 以下の行でエラーが発生する
     err = cl::clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "Error: %i\n", err);
+    __android_log_print(ANDROID_LOG_DEBUG, "OpenCLKeyence", "clBuildProgram result: %i\n", err);
 
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
